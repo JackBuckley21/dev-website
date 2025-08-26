9e9c78b2b8d7f06e47e7eb855566a260aeb8b911
Jack <46270808+JackBuckley21@users.noreply.github.com>
2025-08-26 21:58:23 +0100

    Background animation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,7 +25,7 @@ export const metadata: Metadata = {
         siteName: "Jack Buckley Portfolio",
         images: [
             {
-                url: "/og-image.png",
+                url: "/preview.png",
                 width: 1200,
                 height: 630,
                 alt: "Jack Buckley's Portfolio",
